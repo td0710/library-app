13782eddb8b4c6f65a14a428a09bc704afb99f73
td <thaisduog07@gmail.com>
2024-12-22 19:50:21 +0700

    add routing

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -7,12 +7,28 @@ import { Heros } from "./layouts/HomePage/components/Heros";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/NewBooksPage/NewBooksPage";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <SearchBooksPage></SearchBooksPage>
-      <Footer></Footer>
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home"></Redirect>
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 };
